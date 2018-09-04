fac1752481aec7b5e57cc3cbee177bb4d591fd9f
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2018-09-05 01:06:59 +0200

    Initial PoC and basic projects structure

---
+++ Catharsium.Calendar.Google.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Catharsium.Calendar.Google.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
