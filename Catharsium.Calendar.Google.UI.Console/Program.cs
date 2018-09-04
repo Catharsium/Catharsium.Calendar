064ab37c3732e46bb81206c9e637b26f5ed6b823
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2018-09-05 01:07:33 +0200

    Cleanup

--- Catharsium.Calendar.Google.UI.Console/Program.cs
+++ Catharsium.Calendar.Google.UI.Console/Program.cs
@@ -4,12 +4,8 @@ using Google.Apis.Calendar.v3.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace CalendarQuickstart
 {
