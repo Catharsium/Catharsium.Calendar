ba73ea95efb688af34f3d5ed847ccc13d22d8004
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2018-09-05 21:39:47 +0200

    Added AutoMapper

---
+++ Catharsium.Calendar.Google.Core.Entities/Models/CalendarEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Catharsium.Calendar.Google.Core.Entities.Models
+{
+    public class CalendarEvent
+    {
+        public string Summary { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+    }
+}
---
+++ Catharsium.Calendar.Google/EventMapperFactory.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catharsium.Calendar.Google
+{
+    public class EventMapperFactory
+    {
+        //public IMapper GetInternalToExternalMapper()
+        //{
+        //    var 
+        //}
+    }
+}
