82eb10efb7a3c6b5e31ea749cbb6da622d164ffd
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2018-09-19 17:51:13 +0200

    Add EventMapperFactory

--- Catharsium.Calendar.Google/EventMapperFactory.cs
+++ Catharsium.Calendar.Google/EventMapperFactory.cs
@@ -1,10 +1,13 @@
-﻿namespace Catharsium.Calendar.Google
+﻿using AutoMapper;
+using System;
+
+namespace Catharsium.Calendar.Google
 {
     public class EventMapperFactory
     {
-        //public IMapper GetInternalToExternalMapper()
-        //{
-        //    
-        //}
+        public IMapper GetInternalToExternalMapper()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
