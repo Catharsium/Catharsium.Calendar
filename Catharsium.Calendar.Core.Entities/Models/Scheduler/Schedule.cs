68e2007104d6713921d8bcc23bf8699aac69bc51
Thierry Brachthuizer <thierry.brachthuizer@unifiedpost.com>
2020-08-10 08:23:42 +0200

    Add schedule

---
+++ Catharsium.Calendar.Core.Entities/Models/Scheduler/Schedule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catharsium.Calendar.Core.Entities.Models.Scheduler
+{
+    public class Schedule
+    {
+        public Frequency Frequency { get; set; }
+        public DayOfWeek DayOfWeek { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+
+
+    public enum Frequency
+    {
+        Daily,
+        Weekly,
+        Monthly,
+        Annually
+    }
+}
